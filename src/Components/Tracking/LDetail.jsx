53400c745fe2ca18c5ae7aaf30e0d4bcacdab66d
E V <evanvargas@gmail.com>
2023-09-12 10:26:13 -0500

    Detail Page

--- src/Components/Tracking/LDetail.jsx
+++ src/Components/Tracking/LDetail.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import {
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useSteps,
+  Box,
+  Text,
+  Flex,
+  VStack,
+  useColorModeValue,
+  Divider,
+} from '@chakra-ui/react';
 
-const LDetail = () => {
+const steps = [
+  {
+    title: 'Processed Order',
+    description: 'September 5, 2023: 6:00 AM, California, USA',
+  },
+  {
+    title: 'In Transit',
+    description: 'September 5, 2023: 11:00 AM,  California, USA',
+  },
+  {
+    title: 'Expected Delivery',
+    description: 'September 8, 2023: 6:00 PM, Texas, USA',
+  },
+];
+
+function LDetail() {
+  const { activeStep, setActiveStep } = useSteps({
+    index: 1,
+    count: steps.length,
+  });
+
   return (
-    <div>LDetail</div>
-  )
+    <>
+      <VStack>
+        <Text fontSize="40px" fontWeight="bold" mt="20px">
+          Tracking Details
+        </Text>
+        <Flex alignItems="center" mt="-3">
+          <Text fontSize="16px" fontWeight="semibold">
+            Tracking Number:
+          </Text>
+          <Text fontSize="16px" mx="10px">
+            85462189621354
+          </Text>
+        </Flex>
+      </VStack>
+
+      <VStack>
+        <Flex
+          minW="370px"
+          mb="30px"
+          mt="30px"
+          bg={useColorModeValue('gray.300', 'blue.800')}
+          p="25px"
+          borderRadius="20px"
+          
+        >
+          <Stepper
+            index={activeStep}
+            orientation="vertical"
+            height="400px"
+            gap="0"
+            size="lg"
+            colorScheme='green'
+            
+          >
+            {steps.map((step, index) => (
+              <Step key={index} >
+                <StepIndicator >
+                  <StepStatus 
+                    complete={<StepIcon />}
+                    incomplete={<StepNumber />}
+                    active={<StepNumber fontWeight="bold" />}
+                    
+                  />
+                </StepIndicator>
+
+                <Box flexShrink="0" >
+                  <StepTitle  >{step.title}</StepTitle>
+                  <StepDescription >{step.description}</StepDescription>
+                </Box>
+
+                <StepSeparator />
+              </Step>
+            ))}
+          </Stepper >
+        </Flex>
+      </VStack>
+    </>
+  );
 }
 
-export default LDetail
+export default LDetail;
--- src/Components/Tracking/MDetail.jsx
+++ src/Components/Tracking/MDetail.jsx
@@ -13,17 +13,22 @@ import {
   Text,
   Flex,
   VStack,
+  useColorModeValue,
+  Divider,
 } from '@chakra-ui/react';
 
 const steps = [
   {
     title: 'Processed Order',
-    description: 'September 5, 2023: 6:00 AM, California, USA',
+    description: 'Sept. 5, 2023: 6:00 AM, CA, USA',
   },
-  { title: 'In Transit', description: 'September 5, 2023: 11:00 AM' },
+  {
+    title: 'In Transit',
+    description: 'Sept. 5, 2023: 11:00 AM,  CA, USA',
+  },
   {
     title: 'Expected Delivery',
-    description: 'September 8, 2023: 6:00 PM, Texas, USA',
+    description: 'Sept. 8, 2023: 6:00 PM, TX, USA',
   },
 ];
 
@@ -35,78 +40,58 @@ function MDetail() {
 
   return (
     <>
-      <VStack  mb="20px">
+      <VStack>
         <Text fontSize="25px" fontWeight="bold" mt="20px">
           Tracking Details
         </Text>
-        <Flex alignItems="center">
+        <Flex alignItems="center" mt="-3">
           <Text fontSize="14px" fontWeight="semibold">
-            Shipping Number:
+            Tracking Number:
           </Text>
           <Text fontSize="14px" mx="10px">
-            12345
+            85462189621354
           </Text>
         </Flex>
       </VStack>
 
-      <Flex mx="25px" direction="column" mb="10px">
-        <Text fontSize="20px" fontWeight="semibold" mb="2px">
-          Client Order Information
-        </Text>
-        <Text fontSize="16px">John Doe</Text>
-        <Text fontSize="14px">123 Alphabet St.</Text>
-        <Text fontSize="14px">Houston, TX 77023</Text>
-        <Text fontSize="14px">713-382-5146</Text>
-        <Text fontSize="14px" mb="10px">
-          joey@gmail.com
-        </Text>
-        
-      </Flex>
-      <Flex mx="25px" direction="column" mb="10px">
-        <Text fontSize="20px" fontWeight="semibold" mb="2px">
-          ImEx Cargo
-        </Text>
-        <Text fontSize="16px">Customer Service</Text>
-        <Text fontSize="14px">1R Newbury St</Text>
-        <Text fontSize="14px">Peabody, MA 01960</Text>
-        <Text fontSize="14px">617-718-4639</Text>
-        <Text fontSize="14px" mb="10px">
-          group@imexcargo.com
-        </Text>
-      </Flex>
-
       <VStack>
-        <Text fontSize="25px" fontWeight="semibold" mt="10px" mb="20px">
-          Updates
-        </Text>
-      </VStack>
-
-      <VStack mb="45px">
-        <Stepper
-          index={activeStep}
-          orientation="vertical"
-          height="400px"
-          gap="0"
+        <Flex
+          minW="305px"
+          mb="30px"
+          mt="30px"
+          bg={useColorModeValue('gray.300', 'blue.800')}
+          p="25px"
+          borderRadius="20px"
         >
-          {steps.map((step, index) => (
-            <Step key={index}>
-              <StepIndicator>
-                <StepStatus
-                  complete={<StepIcon />}
-                  incomplete={<StepNumber />}
-                  active={<StepNumber />}
-                />
-              </StepIndicator>
+          <Stepper
+            index={activeStep}
+            orientation="vertical"
+            height="400px"
+            gap="0"
+            colorScheme='green'
+            
+          >
+            {steps.map((step, index) => (
+              <Step key={index} >
+                <StepIndicator >
+                  <StepStatus
+                    complete={<StepIcon />}
+                    incomplete={<StepNumber />}
+                    active={<StepNumber />}
+                    
+                  />
+                </StepIndicator>
 
-              <Box flexShrink="0">
-                <StepTitle>{step.title}</StepTitle>
-                <StepDescription>{step.description}</StepDescription>
-              </Box>
+                <Box flexShrink="0" >
+                  <StepTitle>{step.title}</StepTitle>
+                  <StepDescription>{step.description}</StepDescription>
+                </Box>
 
-              <StepSeparator />
-            </Step>
-          ))}
-        </Stepper>
+                <StepSeparator  />
+              </Step>
+            ))}
+          </Stepper>
+        </Flex>
       </VStack>
     </>
   );
